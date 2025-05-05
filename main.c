17a80f7d35c41bddd13433557c7f05c0214bb17a
marian <marian.muller@serli.com>
2025-05-05 13:18:29 +0200

    use pio

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "joypad.pio.h"
 
  
 #define LATCH_PIN 13
@@ -7,31 +9,27 @@
 #define DATA_PIN 15
 
 
-// Data line is **active LOW** --> unpressed button is HIGH on data line, 0 on SNES CPU
-// Last 4 bits are always 0000 for a standard controller --> HIGH on data line
 // Bits order: B Y Select Start Up Down Left Right / A X L R 0 0 0 0
 typedef struct {
-    uint16_t signature:4;
-    uint16_t r:1;
-    uint16_t l:1;
-    uint16_t x:1;
-    uint16_t a:1;
-    uint16_t right:1;
-    uint16_t left:1;
-    uint16_t down:1;
-    uint16_t up:1;
-    uint16_t start:1;
-    uint16_t select:1;
-    uint16_t y:1;
     uint16_t b:1;
+    uint16_t y:1;
+    uint16_t select:1;
+    uint16_t start:1;
+    uint16_t up:1;
+    uint16_t down:1;
+    uint16_t left:1;
+    uint16_t right:1;
+    uint16_t a:1;
+    uint16_t x:1;
+    uint16_t l:1;
+    uint16_t r:1;
+    uint16_t signature:4;
 } buttons_t;
 
 typedef union {
     buttons_t buttons;
     uint16_t raw;
 } input_t;
-
-#define BIT(n) (!((data.raw >> n) & 0x1))
 
 input_t data = {0};
 
@@ -41,66 +39,44 @@ uint16_t counter = 0;
 // TODO Custom ID for serial? 3 nibbles worth of data? one byte only?
 // TODO or simply ignore controller ID and use both bytes as data???
 
+PIO pio;
+int sm;
+
+static void pio_irq_func(void) {
+    // Push current input to FIFO
+    pio_sm_put_blocking(pio, sm, data.raw);
+    
+    // Press DOWN button every 60 polls
+    counter++;
+    data.buttons.down = (counter % 60) == 0;
+
+    //printf("inputs: 0x%04x down=0x%x\n", data.raw, data.buttons.down);
+}
+
 int main() {
     stdio_init_all();
 
-    // Configure GPIOs
-    gpio_init(LATCH_PIN);
-    gpio_init(CLOCK_PIN);
-    gpio_init(DATA_PIN);
-    gpio_set_dir(DATA_PIN, GPIO_OUT);
+    // Initialize PIO program to answer controller polls from console
+    pio = pio0;
+    sm = pio_claim_unused_sm(pio, true);
+    uint offset = pio_add_program(pio, &joypad_program);
+    joypad_program_init(pio, sm, offset, LATCH_PIN);
 
-    // Requires external 3.6Kohms pull-up resistors on LATCH and CLOCK pins
-    gpio_pull_up(LATCH_PIN);
-    gpio_pull_up(CLOCK_PIN);
+    // Setup IRQ handler
+    int pio_irq = pio_get_irq_num(pio, 0);
+    irq_set_exclusive_handler(pio_irq, pio_irq_func);
+    irq_set_enabled(pio_irq, true);
+    pio_set_irq0_source_enabled(pio, pis_interrupt0 + sm, true);
 
-    // Data line is low at rest
-    gpio_put(DATA_PIN, 0);
+    // Start PIO program
+    pio_sm_set_enabled(pio, sm, true);
+
 
     while(true) {
-
-        // Wait for latch pulse
-        while (gpio_get(LATCH_PIN) == 0) {
-            tight_loop_contents();
-        }
-        while (gpio_get(LATCH_PIN) != 0) {
-            tight_loop_contents();
-        }
-        
-        // Output first bit on latch falling edge
-        gpio_put(DATA_PIN, BIT(15));
-        
-        // Output the next 15 bits
-        for (int i=14; i>=0; i--) {
-            // Wait for next clock pulse
-            while (gpio_get(CLOCK_PIN) != 0) {
-                tight_loop_contents();
-            }
-            while (gpio_get(CLOCK_PIN) == 0) {
-                tight_loop_contents();
-            }
-
-            // Output next bit on clock rising edge
-            gpio_put(DATA_PIN, BIT(i));
-        }
-
-        // Wait for the last clock pulse
-        while (gpio_get(CLOCK_PIN) != 0) {
-            tight_loop_contents();
-        }
-        while (gpio_get(CLOCK_PIN) == 0) {
-            tight_loop_contents();
-        }
-        
-        // Data line is low at rest
-        gpio_put(DATA_PIN, 0);
-
-        // Press DOWN button every 60 polls
-        counter++;
-        data.buttons.down = (counter % 60) == 0;
-
-        //printf("inputs: 0x%04x down=0x%x=%d\n", data.raw, data.buttons.down, BIT(10));
-
+        // Wait for PIO interrupt and push next inputs into FIFO
+        //tight_loop_contents();
+        sleep_ms(2000);
+        printf(".\n");
     }
     return 0;
 }
